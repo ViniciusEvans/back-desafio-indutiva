5b1b55b82442bbd6edf3ba8aad430d142dcd188d
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 23:18:29 -0300

    criando as funções do crud

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -14,16 +14,66 @@ async function createProducts(req, res) {
     if (response === 0) {
       return res
         .status(400)
-        .json({ message: "it was not possible to register a new product" });
+        .json({ error: "it was not possible to register a new product" });
     }
-    return res.status(201).json({ message: "Sucess!" });
+    return res.status(200).json({ success: "Success!" });
   } catch (error) {
     return res.status(400).json(error.message);
   }
 }
-async function getProducts(req, res) {}
-async function updateProducts(req, res) {}
-async function deleteProducts(req, res) {}
+async function getProducts(req, res) {
+  try {
+    const response = await knex("products").returning("*");
+    if (response === 0) {
+      return res.status(404).json({ error: "has nothing in database" });
+    }
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+async function updateProducts(req, res) {
+  const { title, image, amount, description } = req.body.product;
+  const { id } = req.params;
+  try {
+    const productExist = await knex("products")
+      .select("*")
+      .where({ id })
+      .first();
+    if (!productExist) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    const values = {
+      title,
+      image,
+      amount,
+      description,
+    };
+    await knex("products").update(values).where({ id });
+    return res.status(200).json({
+      success: "Product updated with success",
+    });
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+async function deleteProducts(req, res) {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ error: "need id in params" });
+  }
+  try {
+    const productExist = await knex("products").where({ id }).first();
+    console.log(productExist);
+    if (!productExist) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    await knex("products").where({ id }).first().del();
+    return res.status(200).json({ success: "Product deleted with success" });
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
 
 module.exports = {
   createProducts,
