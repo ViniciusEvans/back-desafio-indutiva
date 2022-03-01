7a7065a460a43c8cacb041bcfff58b96998b8ce6
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 22:21:07 -0300

    criando query de de registro de novo produto

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -1,7 +1,29 @@
-function createProducts(req, res) {}
-function getProducts(req, res) {}
-function updateProducts(req, res) {}
-function deleteProducts(req, res) {}
+const knex = require("../../database/connection");
+
+async function createProducts(req, res) {
+  const { title, image, amount, description } = req.body.product;
+  try {
+    const response = await knex("products")
+      .insert({
+        title: title,
+        image: image,
+        amount: amount,
+        description: description,
+      })
+      .returning("*");
+    if (response === 0) {
+      return res
+        .status(400)
+        .json({ message: "it was not possible to register a new product" });
+    }
+    return res.status(201).json({ message: "Sucess!" });
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+async function getProducts(req, res) {}
+async function updateProducts(req, res) {}
+async function deleteProducts(req, res) {}
 
 module.exports = {
   createProducts,
