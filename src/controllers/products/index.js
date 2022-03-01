ff51f9a695931cbfa745b2cab9cde5a3b9d26a6d
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 22:09:28 -0300

    definindo controladores

---
+++ src/controllers/products/index.js
@@ -0,0 +1,11 @@
+function createProducts(req, res) {}
+function getProducts(req, res) {}
+function updateProducts(req, res) {}
+function deleteProducts(req, res) {}
+
+module.exports = {
+  createProducts,
+  getProducts,
+  updateProducts,
+  deleteProducts,
+};
