61399158a7c594dba7f230199fbf7a3d57abb33e
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 23:21:27 -0300

    retirando validação do yup locales

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -12,11 +12,6 @@ async function validationFields(req, res, next) {
     });
     next();
   } catch (error) {
-    if (error.message === "amount deve ser maior ou igual a 0") {
-      return res
-        .status(400)
-        .json({ error: "the amount must be bigger than 0" });
-    }
     return res.status(400).json(error.message);
   }
 }
